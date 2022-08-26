2c1738c44e0e174856cd351e305760ff486f989a
dinushka <dinushkapiyumal678@gmail.com>
2022-08-26 12:59:38 +0530

    add pages react

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes , Route} from "react-router-dom"
+import HomePage from "./pages/user/HomePage/HomePage";
+import Login from "./pages/user/Login/Login";
+import Register from "./pages/user/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
+
 
 export default App;
