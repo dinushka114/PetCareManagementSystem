43262befb1a785d343b4a084dcf75289e5925ca8
madhuminiSGX <madhumini@syntaxgenie.com>
2022-08-28 14:44:53 +0530

    Revert "frontend pages basics"
    This reverts commit ee6ebba0157fb2d30e046653fa1718df34e87fb6.

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/petboarding/admin/Dashboard';
-import Reports from './pages/petboarding/admin/Reports';
-import Update from './pages/petboarding/admin/UpdateBoarding';
-import AddNewBoarding from './pages/petboarding/admin/AddNewBoarding';
-import Client from './pages/petboarding/user/petBoardingPlaces';
 
 function App() {
   return (
-    <>
-    <Router>
-        <Routes>
-          <Route exact path='/' element={<Dashboard/>} />
-          <Route exact path='/add' element={<AddNewBoarding/>} />
-          <Route exact path='/report' element={<Reports/>} />
-          <Route exact path='/update' element={<Update/>} />
-          <Route exact path='/client' element={<Client/>} />
-        </Routes>
-    </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
