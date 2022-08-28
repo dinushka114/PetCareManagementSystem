53199ac0340158062ecc176ef771589f4189aa5f
madda01 <it20232368@my.sliit.lk>
2022-08-28 15:39:34 +0530

    frontend basic folder structure

---
+++ petcare-frontend/src/pages/petboarding/user/petBoardingPlaces.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../../../components/petboarding/user/boardings';
+import Navbar from '../../../components/petboarding/user/sidebar/Cnavbar';
+
+function Dashboard() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    </>
+  )
+}
+
+export default Dashboard
