ee6ebba0157fb2d30e046653fa1718df34e87fb6
madhuminiSGX <madhumini@syntaxgenie.com>
2022-08-28 14:31:40 +0530

    frontend pages basics

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/petboarding/admin/Dashboard';
+import Reports from './pages/petboarding/admin/Reports';
+import Update from './pages/petboarding/admin/UpdateBoarding';
+import AddNewBoarding from './pages/petboarding/admin/AddNewBoarding';
+import Client from './pages/petboarding/user/petBoardingPlaces';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <Routes>
+          <Route exact path='/' element={<Dashboard/>} />
+          <Route exact path='/add' element={<AddNewBoarding/>} />
+          <Route exact path='/report' element={<Reports/>} />
+          <Route exact path='/update' element={<Update/>} />
+          <Route exact path='/client' element={<Client/>} />
+        </Routes>
+    </Router>
+    </>
   );
 }
 
