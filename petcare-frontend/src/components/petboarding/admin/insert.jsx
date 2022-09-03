203e88b1d45316259904d82c51f0e2ecf1a78921
madda01 <it20232368@my.sliit.lk>
2022-09-03 10:00:37 +0530

    add get delete done

--- petcare-frontend/src/components/petboarding/admin/insert.jsx
+++ petcare-frontend/src/components/petboarding/admin/insert.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Badge from 'react-bootstrap/Badge';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import React, { useState } from 'react';
 import './insert.css';
 import image from '../../../images/add.jpg';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import {useNavigate} from 'react-router-dom';
 
-const insertBoardingPlaces = () => {
+const InsertBoardingPlaces = () => {
+
+    const [boardingName,setboardingName] = useState("");
+    const [boardingImage,setboardingImage] = useState("");
+    const [boardingemail,setboardingemail] = useState("");
+    const [boardingaddress, setboardingaddress] = useState("");
+    const [boardingphone,setboardingphone] = useState("");
+    const [openHoursStart,setopenHoursStart] = useState("");
+    const [openHoursEnd,setopenHoursEnd] = useState("");
+
+    const navigate = useNavigate();
+
+    function sendBoarding(e){
+        e.preventDefault();
+    
+        const newBoarding = {
+            boardingName,
+            boardingImage,
+            boardingemail,
+            boardingaddress,
+            boardingphone,
+            openHoursStart,
+            openHoursEnd
+        }
+
+        axios.post("http://localhost:3000/pet-boarding/add",newBoarding).then(()=>{
+            Swal.fire("Boarding added");
+            navigate('/');
+        }).catch((err)=>{
+            alert(err)
+        })    
+    }
+
   return (
     
     <div className="home">
     <div className="homeContainer">
         <h1>Add New Pet Boarding Places</h1>
-        
-            <form className='border border-success rounded'>
-           
+            <form className='border border-success rounded' onSubmit={sendBoarding}>
                 <div className='p-4'>
                     <div className='col-sm-6'>
                         <div className="form-group">
-                            <label htmlFor="boardingname">Pet boarding place name</label>
-                            <input  name="boardingname" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
+                            <label htmlFor="boardingName">Pet boarding place name</label>
+                            <input  name="boardingName" id="boardingName"  cols="10" rows="4" className='form-control'
+                                type="text" placeholder="Pet Care" required
+                                onChange={(e)=>{
+                                    setboardingName(e.target.value);
+                                }}/>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
                         </div>
                     </div>
                     
@@ -28,55 +63,81 @@ const insertBoardingPlaces = () => {
                     <div className='col-sm-6'>
                         <div className="form-group">
                             <div className="form-group">
-                                <label htmlFor="boardingimage">Pet boarding place image</label>
-                                <input type='file' accept="image/png, image/jpeg" name="boardingimage" id=""  cols="10" rows="4" className='form-control'></input>
-                                <p className="text-danger p-1">  
-                                </p>
+                                <label htmlFor="boardingImage">Pet boarding place image</label>
+                                <input type='file' accept="image/png, image/jpeg" name="boardingImage" id="boardingImage"  cols="10" rows="4" className='form-control'
+                                    onChange={(e)=>{
+                                        setboardingImage(e.target.value);
+                                    }}/>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-6'>
                         <div className="form-group">
                             <label htmlFor="boardingemail">Pet boarding place email</label>
-                            <input  name="boardingemail" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
+                            <input  name="boardingemail" id="boardingemail"  cols="10" rows="4" className='form-control' 
+                                type="email" required
+                                onChange={(e)=>{
+                                    setboardingemail(e.target.value);
+                                }}></input>
                         </div>
                     </div>
                     <div className='col-sm-6'>
                         <div className="form-group">
                             <label htmlFor="boardingaddress">Pet boarding place address</label>
-                            <input  name="boardingaddress" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
+                            <input  name="boardingaddress" id="boardingaddress"  cols="10" rows="4" className='form-control'
+                                type="text" required
+                            onChange={(e)=>{
+                                setboardingaddress(e.target.value);
+                            }}></input>
                         </div>
                     </div>
                     <div className='col-sm-6'>
                         <div className="form-group">
-                            <label htmlFor="contactno">Pet boarding place phone no</label>
-                            <input type="text" name="contactno" id="" cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">
-                            </p>
+                            <label htmlFor="boardingphone">Pet boarding place phone no</label>
+                            <input type="text" name="boardingphone" id="boardingphone" cols="10" rows="4" className='form-control'
+                                pattern="[0][0-9]{9}" required
+                            onChange={(e)=>{
+                                setboardingphone(e.target.value);
+                            }}/>
                         </div>
                     </div>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="openhours">Pet boarding place open hours</label>
-                            <DropdownButton variant="secondary" id="dropdown-basic" title="Open Hours">
-                                <Dropdown.Item href="#/action-1">8AM-6PM</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">6PM-8AM</Dropdown.Item>
-                            </DropdownButton>
-                        </div>
-                    </div> 
+                    <div className='row'>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <label htmlFor="openHours">Open hours</label>
+                                    <input type='time' name="openHoursStart" id="openHoursStart" cols="10" rows="4" className='form-control' placeholder='Start Time'
+                                    onChange={(e)=>{
+                                        setopenHoursStart(e.target.value);
+                                    }} />
+                                </div>
+                            </div> 
+                            <div className='col-sm-5'>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1"></label>
+                                    <input type='time' name="openHoursEnd" id="openHoursEnd" cols="10" rows="4" className='form-control' placeholder='End Time'
+                                    onChange={(e)=>{
+                                        setopenHoursEnd(e.target.value);
+                                    }} />
+                                </div>
+                            </div>              
+                            </div>         
                        <br/>
-                       <Badge bg="success" style={{fontSize: 18}}>Save</Badge> 
-                       <Badge bg="danger" style={{fontSize: 18}}>Clear</Badge>
+                       <div className='row'>
+                                <div className='col-sm-2'>
+                                    <input type="submit" name='Save' value='Save' className='btn btn-success'></input> 
+                                </div>
+                                <div className='col-sm-2'>
+                                    <input type="submit" name='Clear' value='Clear' className='btn btn-danger' ></input>
+                                </div>
+                        </div>
                 </div>
             </form>
             <br/>
     </div>
 </div>
+
   )
+ 
 }
 
-export default insertBoardingPlaces;
+export default InsertBoardingPlaces;
--- petcare-frontend/src/components/petboarding/admin/update.jsx
+++ petcare-frontend/src/components/petboarding/admin/update.jsx
@@ -1,82 +1,91 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Badge from 'react-bootstrap/Badge';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import './insert.css';
 import image from '../../../images/update.jpg';
+import { Component } from 'react';
 
-const insertBoardingPlaces = () => {
-  return (
-    
-    <div className="home">
-    <div className="homeContainer">
-        <h1>Update Pet Boarding Places</h1>
-        
-            <form className='border border-success rounded'>
-           
-                <div className='p-4'>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="boardingname">Pet boarding place name</label>
-                            <input  name="boardingname" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
-                        </div>
-                    </div>
-                    
-                    <img src={image} alt='add' style={{width:'45%',float:'right'}}></img>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <div className="form-group">
-                                <label htmlFor="boardingimage">Pet boarding place image</label>
-                                <input type='file' accept="image/png, image/jpeg" name="boardingimage" id=""  cols="10" rows="4" className='form-control'></input>
-                                <p className="text-danger p-1">  
-                                </p>
+export default class insertBoardingPlaces extends Component{
+    render(){
+        return (
+            
+            <div className="home">
+            <div className="homeContainer">
+                <h1>Update Pet Boarding Places</h1>
+                
+                    <form className='border border-success rounded'>
+                
+                        <div className='p-4'>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <label htmlFor="boardingname">Pet boarding place name</label>
+                                    <input  name="boardingname" id=""  cols="10" rows="4" className='form-control'></input>
+                                    <p className="text-danger p-1">   
+                                    </p>
+                                </div>
                             </div>
+                            
+                            <img src={image} alt='add' style={{width:'45%',float:'right'}}></img>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <div className="form-group">
+                                        <label htmlFor="boardingimage">Pet boarding place image</label>
+                                        <input type='file' accept="image/png, image/jpeg" name="boardingimage" id=""  cols="10" rows="4" className='form-control'></input>
+                                        <p className="text-danger p-1">  
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <label htmlFor="boardingemail">Pet boarding place email</label>
+                                    <input  name="boardingemail" id=""  cols="10" rows="4" className='form-control' type="email" required></input>
+                                    <p className="text-danger p-1">   
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <label htmlFor="boardingaddress">Pet boarding place address</label>
+                                    <input  name="boardingaddress" id=""  cols="10" rows="4" className='form-control'  type="text" required></input>
+                                    <p className="text-danger p-1">   
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div className="form-group">
+                                    <label htmlFor="contactno">Pet boarding place phone no</label>
+                                    <input type="text" name="contactno" id="" cols="10" rows="4" className='form-control'  pattern="[0][0-9]{9}" required></input>
+                                    <p className="text-danger p-1">
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='row'>
+                                    <div className='col-sm-6'>
+                                        <div className="form-group">
+                                            <label htmlFor="openHours">Open hours</label>
+                                            <input type='time' name="openHoursStart" id="openHoursStart" cols="10" rows="4" className='form-control' placeholder='Start Time'/>
+                                        </div>
+                                    </div> 
+                                    <div className='col-sm-5'>
+                                        <div className="form-group">
+                                            <label htmlFor="exampleInputPassword1"></label>
+                                            <input type='time' name="openHoursEnd" id="openHoursEnd" cols="10" rows="4" className='form-control' placeholder='End Time'/>
+                                        </div>
+                                    </div>              
+                                    </div>  
+                            <br/>
+                            <div className='row'>
+                                        <div className='col-sm-1'>
+                                            <input type="submit" name='Save' value='Update' className='btn btn-success'></input> 
+                                        </div>
+                                        <div className='col-sm-2'>
+                                            <input type="submit" name='Save' value='Clear' className='btn btn-danger' ></input>
+                                        </div>
+                                </div>
                         </div>
-                    </div>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="boardingemail">Pet boarding place email</label>
-                            <input  name="boardingemail" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
-                        </div>
-                    </div>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="boardingaddress">Pet boarding place address</label>
-                            <input  name="boardingaddress" id=""  cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">   
-                            </p>
-                        </div>
-                    </div>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="contactno">Pet boarding place phone no</label>
-                            <input type="text" name="contactno" id="" cols="10" rows="4" className='form-control'></input>
-                            <p className="text-danger p-1">
-                            </p>
-                        </div>
-                    </div>
-                    <div className='col-sm-6'>
-                        <div className="form-group">
-                            <label htmlFor="openhours">Pet boarding place open hours</label>
-                            <DropdownButton variant="secondary" id="dropdown-basic" title="Open Hours">
-                                <Dropdown.Item href="#/action-1">8AM-6PM</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">6PM-8AM</Dropdown.Item>
-                            </DropdownButton>
-                        </div>
-                    </div> 
-                       <br/>
-                       <Badge bg="success" style={{fontSize: 18}}>Update</Badge> 
-                       <Badge bg="danger" style={{fontSize: 18}}>Clear</Badge>
-                </div>
-            </form>
-            <br/>
-    </div>
-</div>
-  )
+                    </form>
+                    <br/>
+            </div>
+        </div>
+        )
+    }
 }
-
-export default insertBoardingPlaces;
