71663ca733e7ea077dbd3326b3caa6b069040307
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-05 22:16:43 +0530

    added multiple images into Home page

--- petcare-frontend/src/component/Home/Home.js
+++ petcare-frontend/src/component/Home/Home.js
@@ -33,6 +33,13 @@ const Home = () => {
 
         <div className = "container" id = "container">
             <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
+            <Product product = {product} />
         </div>
 
         </Fragment>
--- petcare-frontend/src/component/Home/Product.js
+++ petcare-frontend/src/component/Home/Product.js
@@ -4,6 +4,11 @@ import ReactStars from 'react-rating-stars-component';
 
 const options = {
     edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    size: window.innerWidth <600 ? 20 : 25,
+    value: 2.5,
+    isHalf: true,
 };
 
 function Product( {product}) {
