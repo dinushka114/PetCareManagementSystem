0d39e10bfca77cbe5f280f6b24deafab3a265a90
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-05 13:33:52 +0530

    create Home folder

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import WebFont from "webfontloader";
 import React from 'react';
-import Footer from "./component/layout/Footer/Footer.js"
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js"
 
 function App() {
 React.useEffect(() => {
@@ -19,6 +20,7 @@ React.useEffect(() => {
   <Router>
    <Header />
 
+    <Route exact path = "/" component={Home}/>
    <Footer />
    </Router>
   );
