1037c95843b4911177ff7052f6a581e37f2cec11
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-06 12:58:57 +0530

    updated store.js

--- petcare-frontend/src/component/layout/Footer/Footer.js
+++ petcare-frontend/src/component/layout/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer id = "footer">
 
 
-        <div class = "leftFooter">
+        <div className = "leftFooter">
             <h4> Download Our App </h4>
             <p> Download App for Android and IOS Mobile Phone </p>
             <img src={playstore} alt="PlayStore" />
@@ -17,7 +17,7 @@ const Footer = () => {
 
 
 
-        <div class = "midFooter">
+        <div className = "midFooter">
             <h1> Pet Accessories. </h1>
             <p> High quality is our first priority </p>
             <p> Copyrights 2021 &copy; MeSoori </p>
@@ -25,7 +25,7 @@ const Footer = () => {
 
 
 
-        <div class = "rightFooter">
+        <div className = "rightFooter">
             <h4> Follow Us </h4>
             <a href = "https://www.instagram.com/petmatepetproducts/"> Instagram </a>
             <a href = "https://www.facebook.com/petproductsdirectory"> Facebook </a>
