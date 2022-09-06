89a1504e2f28c85c59fd0ee710003cb1cb94cfde
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-06 20:47:00 +0530

    create folder productAction and updated

---
+++ petcare-frontend/src/actions/productAction.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+import {
+    ALL_PRODUCT_FAIL,
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    CLEAR_ERRORS,
+} from '../constants/productConstants';
+
--- petcare-frontend/src/store.js
+++ petcare-frontend/src/store.js
@@ -1,19 +1,15 @@
-import { createStore , combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-const reducer = combineReducers ({
-
+import { productReducer } from './reducers/productReducer';
+const reducer = combineReducers({
+    products: productReducer,
 });
-
 let initialState = {};
-
 const middleware = [thunk];
-
 const store = createStore(
     reducer,
     initialState,
-    composeWithDevTools (applyMiddleware (...middleware))
+    composeWithDevTools (applyMiddleware(...middleware))
 );
-
 export default store;
