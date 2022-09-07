bcc15f3e8280a567552297d78d7a5a9ba2ad7b41
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-07 16:12:28 +0530

    create add to cart button

--- petcare-frontend/src/component/Product/ProductDetails.js
+++ petcare-frontend/src/component/Product/ProductDetails.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Header from '../layout/Header/Header'
+import Footer from '../layout/Footer/Footer'
 import axios from "axios"
 import { ThemeContext } from '@emotion/react';
 import { Button, CardContent, CardMedia, ownerDocument, TableContainer, useEventCallback } from '@mui/material';
 import Cards from '@mui/material/Card';
 import Paper from "@mui/material/Paper";
 import './ProductDetails.css';
+import Carousel from 'react-material-ui-carousel';
 
 const ProductDetails = () => {
 
@@ -47,24 +50,71 @@ const ProductDetails = () => {
     })
 
     return (
-        <Cards>
-            <CardMedia>
+        <div>
+            <Header />
+        <Fragment>
+            
+            <div className = 'ProductDetails'>
+                
+                <div>
+            <Carousel>
                 {
 
 
-                    <div className='card'>
-                        <img className = 'card-img-top' src = {productData.productImage} />
-                        <div className='container2'> {productData.price} </div>
-                        <button onClick={() => { loadData() }} className='view-button'>ADD TO CART</button>
+                    
+                        <img className = 'CarouselImage' src = {productData.productImage} />
+                        
+                    
+                }
+            </Carousel>
+
+            </div>
+
+                <div>
+                    <div className = 'detailsBlock-1'>
+                    <h2> {productData.productName} </h2>
+                </div>
+
+                <div className = 'detailsBlock-3'>
+                    <h1> {productData.price} </h1>
+
+                    <div className = 'detailsBlock-3-1'>
+                        <div className = 'detailsBlock-3-1-1'>
+                            <button> - </button>
+                            <input value = "1" type = "number" />
+                            <button> + </button>
+                        </div> {" "}
+                        <button> ADD TO CART </button>
                     </div>
 
+                    <p>
+                        Status : {" "}
+                        <b className = {productData.stocks < 1 ? "redColor" : "greenColor"}> 
+                        {productData.stocks <1 ? "OutOfStock" : "InStock"}
+                        </b>
+                    </p>
 
+                </div>
 
-                }
-            </CardMedia>
-        </Cards>
-    )
-}
+                <div className = 'detailsBlock-4'>
+                    Description : <p> {productData.description} </p>
+                </div>
+
+                <button className = 'buyNow'> BUY IT NOW </button>
+
+                
+                
+            </div>
+            
+            </div>
+            
+            <Footer />
+            
+        </Fragment>
+        </div>
+        
+    );
+};
 
 
 export default ProductDetails;
