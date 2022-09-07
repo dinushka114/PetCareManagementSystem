2b8bade52eac1162c692173ebc28fcb2b85a8846
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-07 18:20:15 +0530

    create Dashboard.js file

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -5,8 +5,8 @@ import WebFont from "webfontloader";
 import React from 'react';
 import Footer from "./component/layout/Footer/Footer.js";
 import Home from './component/Home/Home';
-import ProductDetails from './component/Product/ProductDetails.js'
-
+import ProductDetails from './component/Product/ProductDetails.js';
+import Dashboard from './component/admin/Dashboard.js';
 
 
 function App() {
@@ -24,7 +24,7 @@ React.useEffect(() => {
     <Routes>
     <Route path = '/' element = {<Home />}></Route>
     <Route path = '/:id' element = {<ProductDetails />}></Route>
-    
+    <Route path = '/admin/dashboard' element = {<Dashboard />}></Route>
     
     </Routes>
    </Router>
