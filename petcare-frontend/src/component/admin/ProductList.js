fbe6d47d987115f77911ac4055c3be2c12bc902a
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-09-16 13:36:37 +0530

    done print report

--- petcare-frontend/src/component/admin/ProductList.js
+++ petcare-frontend/src/component/admin/ProductList.js
@@ -1,3 +1,5 @@
+import React, { useRef } from 'react'
+import { useReactToPrint } from 'react-to-print'
 import './productList.css'
 import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
@@ -8,83 +10,111 @@ import Sidebar from './Sidebar';
 import Swal from 'sweetalert2';
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
-import React from 'react'
-
 
 
 const ProductList = () => {
-    const [productData,setProductData] = useState([]);
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: 'product-data',
+    onAfterPrint: () => alert('Print Success')
+  });
 
-    const getProductData = () => {
-        axios.get('http://localhost:3000/productRoute/get-product')
-            .then(res => {
-                const allProductData = res.data.result;
-                setProductData(allProductData)       
-            })
-    }
+  const [productData, setProductData] = useState([]);
 
-    const deleteProduct = (id)=>{
-      
-       
-          axios.delete("http://localhost:3000/productRoute/delete-product/"+id)
-          .then(res=>{
-            Swal.fire("Product Deleted Successfully!!");
-            if(res.status===200){
-             // Swal.fire("Product Deleted")
-              getProductData()
-            }
-          })
-      }
+  const getProductData = () => {
+    axios.get('http://localhost:3000/productRoute/get-product')
+      .then(res => {
+        const allProductData = res.data.result;
+        setProductData(allProductData)
+      })
+  }
 
-    useEffect(()=>{
-        getProductData()
-    },[]);
+  const deleteProduct = (id) => {
 
-    return(
-        <div className = "dashboard">
-            <Sidebar />
-         
-        <TableContainer component={Paper} className="table">
-            <div className='productListContainer'>
-        <h1 id="productListHeading">ALL PRODUCTS</h1>
-        <table  className="table table-striped" id="products">
-            
-          <thead>
-            <tr >
-              <th scope="col">Product ID</th>
-              <th scope="col">Product Image</th>
-              <th scope="col">Product Name</th>
-              <th scope="col">Stocks</th>
-              <th scope="col">Price</th>
-              <th scope="col">Description</th>
-              <th colSpan={2}>Action</th>
 
-            </tr>
-          </thead>
-          <tbody>
-            {
-              productData.map((products) => {
-                return (
-                  <tr>
-                    <td><center>{products._id}</center></td>
-                    <td><center><img className = 'image' src = {products.productImage} /> </center> </td>
-                    <td><center>{products.productName}</center></td>
-                    <td><center>{products.stocks} </center></td>
-                    <td><center>{products.price}</center></td>
-                    <td className="width"><center><textarea cols='45' rows='2'>{products.description.substring(0,150)}</textarea></center> </td>
-                    <td><center><Link to = {`/admin/update-product/${products._id}`}><EditIcon className='edit' /></Link></center></td> 
-                    <td><center><DeleteIcon className='delete' onClick={()=>deleteProduct(products._id)} /></center></td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+    axios.delete("http://localhost:3000/productRoute/delete-product/" + id)
+      .then(res => {
+        Swal.fire("Product Deleted Successfully!!");
+        if (res.status === 200) {
+          // Swal.fire("Product Deleted")
+          getProductData()
+        }
+      })
+  }
+
+  useEffect(() => {
+    getProductData()
+  }, []);
+
+  return (
+
+    <div className="dashboard">
+      <Sidebar />
+
+
+
+
+
+
+      <TableContainer component={Paper} className="table">
+        <div className='productListContainer'>
+          <h1 id="productListHeading">ALL PRODUCTS</h1>
+          <input type='' className='search-product' placeholder='Search Product' />
+          <button onClick={handlePrint} className='printBtn'>Print</button>
+
+          <div ref={componentRef} style={{ width: '100%', height: window.innerHeight }}>
+          <table className="table table-striped" id="products">
+
+            <thead>
+              <tr >
+                <th scope="col">Product ID</th>
+                <th scope="col">Product Image</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Stocks</th>
+                <th scope="col">Price</th>
+                <th scope="col">Description</th>
+                <th colSpan={2}>Action</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              {
+                productData.map((products) => {
+                  return (
+                    <tr>
+                      <td><center>{products._id}</center></td>
+                      <td><center><img className='image' src={products.productImage} /> </center> </td>
+                      <td><center>{products.productName}</center></td>
+                      <td><center>{products.stocks} </center></td>
+                      <td><center>{products.price}</center></td>
+                      <td className="width"><center><textarea cols='45' rows='2'>{products.description.substring(0, 150)}</textarea></center> </td>
+                      <td><center><Link to={`/admin/update-product/${products._id}`}><EditIcon className='edit' /></Link></center></td>
+                      <td><center><DeleteIcon className='delete' onClick={() => deleteProduct(products._id)} /></center></td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
         </div>
+        </div >
       </TableContainer>
-      </div>
-       
-    );
-}
+
+
+    </div>
+    
+   
+
+     
+    
+
+    
+
+    
+
+    
+  );
+};
 
 export default ProductList;
