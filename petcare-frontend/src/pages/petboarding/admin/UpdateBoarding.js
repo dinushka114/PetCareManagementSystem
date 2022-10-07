c7d9748bc09fb2a5fc7ff364532121f628086223
madda01 <it20232368@my.sliit.lk>
2022-10-07 23:33:03 +0530

    adding missing boarding files

---
+++ petcare-frontend/src/pages/petboarding/admin/UpdateBoarding.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Update from '../../../components/petboarding/admin/update';
+import Navbar from '../../../components/petboarding/admin/sidebar/Navbar';
+
+function AddNewBoarding() {
+  return (
+    <>
+    <Navbar/>
+    <Update/>
+    </>
+  )
+}
+
+export default AddNewBoarding
