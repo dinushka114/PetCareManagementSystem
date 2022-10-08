fc8f156a73b2e02d9a70c13d99129a07c48d60fe
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-10-08 12:51:31 +0530

    update App.js

--- petcare-frontend/src/App.js
+++ petcare-frontend/src/App.js
@@ -6,10 +6,10 @@ import ServiceHome from './pages/admin/PetServiceAdmin/service'
 import AddService from './pages/admin/PetServiceAdmin/addServiceHome'
 import Card from './components/user/Card/Card'
 import MoreService from './pages/user/moreService'
-import Report from './pages/admin/PetServiceAdmin/MainReport'
+import PetServiceReport from './pages/admin/PetServiceAdmin/MainReport'
 import UpdateServiceHome from './pages/admin/PetServiceAdmin/updateServiceHome';
 
-import { Routes, Route } from "react-router-dom"
+
 import MainDashboard from "./pages/user/DashBoard/DashBoard";
 import HomePage from "./pages/user/HomePage/HomePage";
 import Login from "./pages/user/Login/Login";
@@ -24,6 +24,12 @@ import Cart from "./component/Cart/Cart.js";
 import Report from './component/admin/Report.js';
 
 
+import InsertBoardingPlaces from './components/petboarding/admin/insert';
+import Client from './pages/petboarding/user/petBoardingPlaces';
+import UpdateBoardingPlaces from './components/petboarding/admin/update'
+import petBoardingPlaces from './components/petboarding/admin/home'
+
+
 
 function App() {
 
@@ -34,16 +40,16 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard/*" element={<MainDashboard />} />
       <Route exact path='/pet-boarding' element={<Dashboard />} />
-      <Route exact path='/add' element={<AddNewBoarding />} />
+      <Route exact path='/add' element={<InsertBoardingPlaces />} />
       <Route exact path='/report' element={<Reports />} />
-      <Route exact path='/update/:id' element={<Update />} />
-      <Route exact path='/boarding' element={<Client />} />
+      <Route exact path='/update/:id' element={<UpdateBoardingPlaces />} />
+      <Route exact path='/boarding' element={<petBoardingPlaces />} />
       <Route path="/pet-services" element={<UserHome/>} />
         <Route path="/get-service" element={<ServiceHome/>} />
         <Route path="/add-service" element={<AddService/>} />
         <Route path="/card" element={<Card/>} />
         <Route path="/:id" element={<MoreService/>}></Route>
-        <Route path="/reports" element={<Report/>}></Route>
+        <Route path="/reports" element={<PetServiceReport/>}></Route>
         <Route path="/update-service/:id" element={<UpdateServiceHome/>}></Route>
         
         <Route path='/:id' element={<ProductDetails />}></Route>
@@ -53,6 +59,7 @@ function App() {
         <Route path="/admin/update-product/:id" element={<UpdateProduct />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/admin/report" element={<Report />}></Route>
+        
     </Routes>
 
   );
--- petcare-frontend/src/index.js
+++ petcare-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
